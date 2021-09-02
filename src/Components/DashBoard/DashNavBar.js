615f055efc4abc49d053ed71c6b59727ec6c69ff
BOGUEH <85343204+BOGUEH@users.noreply.github.com>
2021-09-02 17:44:30 +0100

    Add files via upload

---
+++ src/Components/DashBoard/DashNavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DashNavbar.css'
+
+
+export default function DashNavBar() {
+    return (
+        <div>
+            
+            <div className="sidebar">  <img src="../images/emergency-reporting-logo.svg" alt="logo" /></div>
+           
+           <div > 
+                <h3 className="heading">Overview</h3>
+       
+            <img src="../images/bell.svg" alt="logo" />
+            <img src="../images/Ellipse 2.svg" alt="logo" />
+            
+            <p>Admin</p>
+            <img src="../images/Vector.svg" alt="logo" /> 
+          </div>
+           
+
+
+
+        </div>
+    )
+}
